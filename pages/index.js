f855b6ee9caae49713e5a41b771828884d37f75e
imevanc <evan.charalampidis@gmail.com>
2022-09-03 19:40:19 +0100

    outer layer

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,13 @@
 export default function Home() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <div className="flex justify-center items-center flex-col h-screen mt--20 mb-50">
+      <div className="shadow-xl bg-black rounded-sm max-w-full w-{200} min-h-480 relative">
+        <div className="absolute top-0 h-full">
+          <div>1</div>
+          <div>1</div>
+          <div>1</div>
+        </div>
+      </div>
+    </div>
+  );
 }
