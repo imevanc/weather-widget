abb7756e23c186e642116d31e184ba14dd85e744
imevanc <evan.charalampidis@gmail.com>
2022-09-05 13:44:06 +0100

    changed the font size of the normal typography

--- components/Typography/Components/Normal.jsx
+++ components/Typography/Components/Normal.jsx
@@ -1,6 +1,6 @@
 const Normal = ({ title }) => {
   return (
-    <div className="self-center text-slate-100 text-xl sm:text-base md:text-lg lg:text-xl xl:text-xl">
+    <div className="self-center text-slate-100 text-base sm:text-base md:text-lg lg:text-xl xl:text-xl">
       {title}
     </div>
   );
